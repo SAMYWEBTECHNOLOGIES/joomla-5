3899335cc1fe899ace4fe35a43db88ec826e31d2
Ankit Kumar Jagetia <akjagetia@gmail.com>
2024-07-17 19:16:56 +0530

    Only published greetings should be considered.

--- com_greetings/site/src/Model/GreetingsModel.php
+++ com_greetings/site/src/Model/GreetingsModel.php
@@ -65,7 +65,7 @@ class GreetingsModel extends ListModel
 				);
 
 			$query->from('`#__greetings` AS a');
-			$query->where($db->q('state') '= 1');
+			$query->where($db->q('state') .'= 1');
 			$query->order('ordering ASC');
 
 
